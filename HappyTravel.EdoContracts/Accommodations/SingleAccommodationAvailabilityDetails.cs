7b406f77cd16acb8e89f8634f4bbdbbb5de535ee
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-12-04 20:21:00 +0500

    Added request and details for single accommodation availability

---
+++ HappyTravel.EdoContracts/Accommodations/SingleAccommodationAvailabilityDetails.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using HappyTravel.EdoContracts.Accommodations.Internals;
+using Newtonsoft.Json;
+
+namespace HappyTravel.EdoContracts.Accommodations
+{
+    public struct SingleAccommodationAvailabilityDetails
+    {
+        [JsonConstructor]
+        public SingleAccommodationAvailabilityDetails(long availabilityId, DateTime checkInDate, DateTime checkOutDate,
+            int numberOfNights, AccommodationDetails accommodationDetails, List<Agreement> agreements)
+        {
+            AvailabilityId = availabilityId;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            NumberOfNights = numberOfNights;
+            AccommodationDetails = accommodationDetails;
+            Agreements = agreements ?? new List<Agreement>(0);
+        }
+
+
+        public long AvailabilityId { get; }
+        public DateTime CheckInDate { get; }
+        public DateTime CheckOutDate { get; }
+        public int NumberOfNights { get; }
+        public AccommodationDetails AccommodationDetails { get; }
+        public List<Agreement> Agreements { get; }
+    }
+}

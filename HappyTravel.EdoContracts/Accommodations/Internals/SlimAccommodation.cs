e411d8e23dfd0d030de8bd01b936606e4643e234
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-07-23 16:36:45 +0500

    Added hotel chain to slim accommodation

--- HappyTravel.EdoContracts/Accommodations/Internals/SlimAccommodation.cs
+++ HappyTravel.EdoContracts/Accommodations/Internals/SlimAccommodation.cs
@@ -8,7 +8,8 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
     public readonly struct SlimAccommodation
     {
         [JsonConstructor]
-        public SlimAccommodation(string id, in SlimLocationInfo location, string name, in Picture picture, AccommodationRatings rating, PropertyTypes propertyType)
+        public SlimAccommodation(string id, in SlimLocationInfo location, string name, in Picture picture, AccommodationRatings rating, PropertyTypes propertyType,
+            string? hotelChain)
         {
             Id = id;
             Location = location;
@@ -16,6 +17,7 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
             Picture = picture;
             Rating = rating;
             PropertyType = propertyType;
+            HotelChain = hotelChain;
         }
 
 
@@ -26,6 +28,7 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
         public AccommodationRatings Rating { get; }
 
         public PropertyTypes PropertyType { get; }
+        public string? HotelChain { get; }
 
 
         public override bool Equals(object? obj) => obj is SlimAccommodation other && Equals(other);

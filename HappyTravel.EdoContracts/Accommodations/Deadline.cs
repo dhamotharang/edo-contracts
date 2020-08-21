4890e2f0f367c7f9c268fc57203ccda5039e87a7
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-08-21 19:03:22 +0500

    Changed deadline format

--- HappyTravel.EdoContracts/Accommodations/Deadline.cs
+++ HappyTravel.EdoContracts/Accommodations/Deadline.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using HappyTravel.EdoContracts.Accommodations.Internals;
+using HappyTravel.EdoContracts.Extensions;
 using Newtonsoft.Json;
 
 namespace HappyTravel.EdoContracts.Accommodations
@@ -10,16 +11,34 @@ namespace HappyTravel.EdoContracts.Accommodations
     public readonly struct Deadline
     {
         [JsonConstructor]
-        public Deadline(DateTime? date, List<CancellationPolicy> policies, List<string> remarks)
+        public Deadline(DateTime? date, List<CancellationPolicy> policies, List<string> remarks, bool isFinal)
         {
             Date = date;
+            IsFinal = isFinal;
             Policies = policies ?? new List<CancellationPolicy>(0);
             Remarks = remarks ?? new List<string>(0);
         }
 
 
         public DateTime? Date { get; }
+        
+        /// <summary>
+        /// If flag is set it means that deadline is actual there and not needed to fetch with a separate request
+        /// </summary>
+        public bool IsFinal { get; }
         public List<CancellationPolicy> Policies { get; }
         public List<string> Remarks { get; }
+        
+        
+        public bool Equals(Deadline other)
+        {
+            return Nullable.Equals(Date, other.Date) && IsFinal == other.IsFinal && Policies.SafeSequenceEqual(other.Policies) && Remarks.SafeSequenceEqual(other.Remarks);
+        }
+
+
+        public override bool Equals(object? obj) => obj is Deadline other && Equals(other);
+
+
+        public override int GetHashCode() => HashCode.Combine(Date, IsFinal, Policies, Remarks);
     }
 }
--- HappyTravel.EdoContracts/Accommodations/Internals/CancellationPolicy.cs
+++ HappyTravel.EdoContracts/Accommodations/Internals/CancellationPolicy.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using Newtonsoft.Json;
 
 namespace HappyTravel.EdoContracts.Accommodations.Internals
 {
     [StructLayout(LayoutKind.Auto)]
     public readonly struct CancellationPolicy
     {
+        [JsonConstructor]
         public CancellationPolicy(DateTime fromDate, double percentage)
         {
             FromDate = fromDate;
@@ -15,5 +17,14 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
 
         public DateTime FromDate { get; }
         public double Percentage { get; }
+        
+        
+        public bool Equals(CancellationPolicy other) => FromDate.Equals(other.FromDate) && Percentage.Equals(other.Percentage);
+
+
+        public override bool Equals(object? obj) => obj is CancellationPolicy other && Equals(other);
+
+
+        public override int GetHashCode() => HashCode.Combine(FromDate, Percentage);
     }
 }

3445b67e30cf01011d4e92a55b54bd852afb4db0
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-11-19 20:59:46 +0300

    Add Equals and GetHashCode methods into Discount

--- HappyTravel.EdoContracts/General/Discount.cs
+++ HappyTravel.EdoContracts/General/Discount.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 
 namespace HappyTravel.EdoContracts.General
@@ -11,7 +12,16 @@ namespace HappyTravel.EdoContracts.General
             Description = description;
         }
 
+        
+        public override bool Equals(object? obj) => obj is Discount other && Equals(other);
 
+        
+        public bool Equals(in Discount other) => (Percent, Description).Equals((other.Percent, other.Description));
+                
+        
+        public override int GetHashCode() => HashCode.Combine(Percent, Description);
+
+        
         /// <summary>
         ///     The discount description.
         /// </summary>

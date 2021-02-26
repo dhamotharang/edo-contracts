04b13f8bf6733a0ae3b005c3d78b6c7ebd682167
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-02-26 13:26:14 +0600

    Added DirectConnectivity constant

---
+++ HappyTravel.EdoContracts/Accommodations/Constants/CommonSystemTags.cs
@@ -0,0 +1,10 @@
+﻿namespace HappyTravel.EdoContracts.Accommodations.Constants
+{
+    public class CommonSystemTags
+    {
+        /// <summary>
+        ///     The direct connectivity tag.
+        /// </summary>
+        public const string DirectConnectivity = "DirectConnectivity";
+    }
+}

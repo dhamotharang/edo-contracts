1ec64d7c4631dc273e30456a9aca7bd326cc9ae7
Raibo <raibo@live.ru>
2021-05-25 12:45:51 +0500

    Added SynxisId to UniqueAccommodationCodes

--- HappyTravel.EdoContracts/Accommodations/Internals/UniqueAccommodationCodes.cs
+++ HappyTravel.EdoContracts/Accommodations/Internals/UniqueAccommodationCodes.cs
@@ -5,9 +5,10 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
     public readonly struct UniqueAccommodationCodes
     {
         [JsonConstructor]
-        public UniqueAccommodationCodes(string? giataId = null)
+        public UniqueAccommodationCodes(string? giataId = null, string? synxisId = null)
         {
             GiataId = giataId;
+            SynxisId = synxisId;
         }
 
 
@@ -15,5 +16,10 @@ namespace HappyTravel.EdoContracts.Accommodations.Internals
         ///     The GIATA code, more information at https://www.giata.com/en/products-services/hotel-mapping-for-otas/
         /// </summary>
         public string? GiataId { get; }
+
+        /// <summary>
+        ///     The Synxis code
+        /// </summary>
+        public string? SynxisId { get; }
     }
 }
